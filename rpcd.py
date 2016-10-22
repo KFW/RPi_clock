2cb9ca4047a1a37eb27f013d5724f9efdd6711d5
Keith Woeltje <comm@woeltje.org>
2016-10-21 21:29:01 -0500

    Display day and date
    need to fix spacing

--- rpcd.py
+++ rpcd.py
@@ -10,17 +10,21 @@ def tick(time1=''):
     # if time string has changed, update it
     if time2 != time1:
         time1 = time2
+        today = dt.date.today()
+        calendar.config(text=today.strftime("%a, %x"))
+        calendar.grid(row=0, column=0)
         clock.config(text=time2)
-        clock.grid(row=0)
-        countdown = bjcmg - dt.date.today()
+        clock.grid(row=0, column=1)
+        countdown = bjcmg - today
         cd = str(countdown.days)
-        count.config(text='BJC goes live in ' + cd + ' days!')
+        count.config(text='BJCMG goes live in ' + cd + ' days!')
         count.grid(row=1)
     # calls itself every 200 milliseconds
     # to update the time display as needed
     clock.after(200, tick)
 root = tk.Tk()
 root.configure(background='#7ea0d6')
+calendar = tk.Label(root, font=('helvetica', 75, 'bold'), bg='#7ea0d6')
 clock = tk.Label(root, font=('helvetica', 75, 'bold'), bg='#7ea0d6')
 count = tk.Label(root, font=('helvetica', 75, 'bold'), bg='#7ea0d6')
 
