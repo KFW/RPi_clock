fa1b52d4d311d7c8c852c46583bea333420779b1
Keith Woeltje <comm@woeltje.org>
2016-10-22 21:26:49 -0500

    Split display into 3 lines total
    Need to fix spacing on top line, and center countdown days

--- rpcd.py
+++ rpcd.py
@@ -12,18 +12,21 @@ def tick(time1=''):
         time1 = time2
         today = dt.date.today()
         calendar.config(text=today.strftime("%a, %x"))
-        calendar.grid(row=0, column=0)
+        calendar.grid(row=0, column=0, sticky=tk.W)
         clock.config(text=time2)
-        clock.grid(row=0, column=1)
+        clock.grid(row=0, column=1, sticky=tk.E)
         countdown = bjcmg - today
         cd = str(countdown.days)
-        count.config(text='BJCMG goes live in ' + cd + ' days!')
-        count.grid(row=1)
+        tk.Label(root, text='BJCMG goes live in:', font=('helvetica', 75, 
+            'bold'), bg='#7ea0d6').grid(row=1, columnspan=2, sticky=tk.W)
+        count.config(text= cd + ' days!')
+        count.grid(row=2,columnspan=2, sticky=tk.W)
     # calls itself every 200 milliseconds
     # to update the time display as needed
     clock.after(200, tick)
 root = tk.Tk()
 root.configure(background='#7ea0d6')
+root.title = 'HIP Countdown Clock'
 calendar = tk.Label(root, font=('helvetica', 75, 'bold'), bg='#7ea0d6')
 clock = tk.Label(root, font=('helvetica', 75, 'bold'), bg='#7ea0d6')
 count = tk.Label(root, font=('helvetica', 75, 'bold'), bg='#7ea0d6')
