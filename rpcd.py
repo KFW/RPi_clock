9e772a493544233f218a04f62edb907b625eb38b
Keith Woeltje <comm@woeltje.org>
2016-10-23 19:48:57 -0500

    change fonts and window size for RPi

--- rpcd.py
+++ rpcd.py
@@ -17,19 +17,20 @@ def tick(time1=''):
         clock.grid(row=0, column=1, sticky=tk.E)
         countdown = bjcmg - today
         cd = str(countdown.days)
-        tk.Label(root, text='BJCMG goes live in:', font=('helvetica', 75, 
-            'bold'), bg='#7ea0d6').grid(row=1, columnspan=2, sticky=tk.W)
         count.config(text= cd + ' days!')
-        count.grid(row=2,columnspan=2, sticky=tk.W)
+        count.grid(row=2,columnspan=2)
     # calls itself every 200 milliseconds
     # to update the time display as needed
     clock.after(200, tick)
 root = tk.Tk()
 root.configure(background='#7ea0d6')
+root.geometry('790x470+5+5')
 root.title = 'HIP Countdown Clock'
-calendar = tk.Label(root, font=('helvetica', 75, 'bold'), bg='#7ea0d6')
-clock = tk.Label(root, font=('helvetica', 75, 'bold'), bg='#7ea0d6')
-count = tk.Label(root, font=('helvetica', 75, 'bold'), bg='#7ea0d6')
+tk.Label(root, text='BJCMG goes live in:', font=('helvetica', 48, 
+            'bold'), bg='#7ea0d6').grid(row=1, columnspan=2, sticky=tk.W)
+calendar = tk.Label(root, font=('helvetica', 36), bg='#7ea0d6')
+clock = tk.Label(root, font=('helvetica', 48, 'bold'), bg='#7ea0d6')
+count = tk.Label(root, font=('helvetica', 48, 'bold'), bg='#7ea0d6')
 
 # clock.pack(fill='both', expand=1)
 tick()
