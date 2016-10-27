e002555b7f36065879497ea6020abda856861537
Keith Woeltje <comm@woeltje.org>
2016-10-26 20:39:44 -0500

    fix spacing and change font sized
    gave column a weight so things would center properly.

--- rpcd.py
+++ rpcd.py
@@ -2,6 +2,7 @@ import time
 import tkinter as tk
 import datetime as dt
 
+window_size = '800x480'
 bjcmg = dt.date(2017,6,1)
 boone = dt.date(2017,8,5)
 
@@ -20,25 +21,26 @@ def tick(time1=''):
         countdown2 = boone - today
         cd1 = str(countdown1.days)
         cd2 = str(countdown2.days)
-        count1.config(text= cd1 + ' days!')
+        count1.config(text= cd1 + ' days')
         count1.grid(row=2,columnspan=2)
-        count2.config(text= cd2 + ' days!')
+        count2.config(text= cd2 + ' days')
         count2.grid(row=4,columnspan=2)        
     # calls itself every 200 milliseconds
     # to update the time display as needed
     clock.after(200, tick)
 root = tk.Tk()
 root.configure(background='#7ea0d6')
-root.geometry('790x470+5+5')
+root.geometry(window_size)
+root.columnconfigure(1, weight=1)
 root.title('HIP Countdown Clock')
-tk.Label(root, text='BJCMG goes live in:', font=('helvetica', 36, 
+tk.Label(root, text='BJCMG goes live in:', font=('helvetica', 48, 
             'bold'), bg='#7ea0d6').grid(row=1, columnspan=2)
-tk.Label(root, text='Boone goes live in:', font=('helvetica', 36, 
+tk.Label(root, text='Boone goes live in:', font=('helvetica', 48, 
             'bold'), bg='#7ea0d6').grid(row=3, columnspan=2)
-calendar = tk.Label(root, font=('helvetica', 24), bg='#7ea0d6')
-clock = tk.Label(root, font=('helvetica', 72, 'bold'), bg='#7ea0d6', fg='blue4')
-count1 = tk.Label(root, font=('helvetica', 42, 'bold'), bg='#7ea0d6')
-count2 = tk.Label(root, font=('helvetica', 42, 'bold'), bg='#7ea0d6')
+calendar = tk.Label(root, font=('helvetica', 48), bg='#7ea0d6')
+clock = tk.Label(root, font=('helvetica', 80, 'bold'), bg='#7ea0d6', fg='blue4')
+count1 = tk.Label(root, font=('helvetica', 64, 'bold'), bg='#7ea0d6')
+count2 = tk.Label(root, font=('helvetica', 64, 'bold'), bg='#7ea0d6')
 
 # clock.pack(fill='both', expand=1)
 tick()
