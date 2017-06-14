3859652c795efde4c9a156cebf50f4bed64873cd
Keith Woeltje <comm@woeltje.org>
2017-06-14 17:28:05 -0500

    update MG date and test auto roll over
    rolls over to next site
    will break after academic

--- rpcd.py
+++ rpcd.py
@@ -3,8 +3,13 @@ import tkinter as tk
 import datetime as dt
 
 window_size = '800x480'
-bjcmg = dt.date(2017,6,1)
-boone = dt.date(2017,8,5)
+go_lives = [('BJCMG', dt.date(2017,6,1)),
+            ('Boone', dt.date(2017,8,5)),
+            ('Pod 1', dt.date(2017,12,2)), 
+            ('Pod 2', dt.date(2018,2,3)),
+            ('Academic', dt.date(2018,6,2)),
+            ('Memorial', dt.date(2018,10,1))                      
+           ]
 
 def tick(time1=''):
     # get the current local time from the PC
@@ -17,12 +22,24 @@ def tick(time1=''):
         calendar.grid(row=0, column=0, sticky=tk.W)
         clock.config(text=time2)
         clock.grid(row=0, column=1, sticky=tk.E)
-        countdown1 = bjcmg - today
-        countdown2 = boone - today
+        for i in range(len(go_lives)): # using regular iteration would be hard 
+                                # since I need next item as well
+                                # this will break after academic go-live,
+                                # but will do for now
+            if go_lives[i][1] > today:
+                countdown1 = go_lives[i][1] - today
+                h1 = go_lives[i][0]
+                countdown2 = go_lives[i+1][1] - today
+                h2 = go_lives[i+1][0]
+                break
         cd1 = str(countdown1.days)
         cd2 = str(countdown2.days)
+        hosp1.config(text= h1 + ' goes live in:')
+        hosp1.grid(row=1, columnspan=2)
         count1.config(text= cd1 + ' days')
         count1.grid(row=2,columnspan=2)
+        hosp2.config(text= h2 + ' goes live in:')
+        hosp2.grid(row=3, columnspan=2)
         count2.config(text= cd2 + ' days')
         count2.grid(row=4,columnspan=2)        
     # calls itself every 200 milliseconds
@@ -33,13 +50,11 @@ root.configure(background='#7ea0d6')
 root.geometry(window_size)
 root.columnconfigure(1, weight=1)
 root.title('HIP Countdown Clock')
-tk.Label(root, text='BJCMG goes live in:', font=('helvetica', 24, 
-            'bold'), bg='#7ea0d6').grid(row=1, columnspan=2)
-tk.Label(root, text='Boone goes live in:', font=('helvetica', 24, 
-            'bold'), bg='#7ea0d6').grid(row=3, columnspan=2)
 calendar = tk.Label(root, font=('helvetica', 32), bg='#7ea0d6')
 clock = tk.Label(root, font=('helvetica', 80, 'bold'), bg='#7ea0d6', fg='blue4')
+hosp1 = tk.Label(root, font=('helvetica', 24, 'bold'), bg='#7ea0d6')
 count1 = tk.Label(root, font=('helvetica', 48, 'bold'), bg='#7ea0d6')
+hosp2 = tk.Label(root, font=('helvetica', 24, 'bold'), bg='#7ea0d6')
 count2 = tk.Label(root, font=('helvetica', 48, 'bold'), bg='#7ea0d6')
 
 # clock.pack(fill='both', expand=1)
