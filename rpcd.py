e81eccd75d21012d1e4ab3fb17c462cdef6c3210
Keith Woeltje <comm@woeltje.org>
2018-05-28 13:01:07 -0500

    clean up formatting
    remove old go lives; change date and time display; have '???' if go live date uncertain

--- rpcd.py
+++ rpcd.py
@@ -3,16 +3,11 @@ import tkinter as tk
 import datetime as dt
 
 window_size = '800x480'
-go_lives = [#('BJCMG', dt.date(2017,6,3)),
-            #('Boone', dt.date(2017,8,5)),
-            #('Pod 1', dt.date(2017,12,2)), 
-            ('Pod 2', dt.date(2018,2,3)),
-            ('Academic', dt.date(2018,6,2)),
-            ('Memorial Med Grp (maybe)', dt.date(2019,4,1)),
-            ('Memorial (maybe)', dt.date(2019,4,1)),
-            ('???', dt.date(2021,4,1)),
-            ('????', dt.date(2025,4,1))                     
+go_lives = [('Academic', dt.date(2018,6,2)),
+            ('Memorial Med Group', dt.date(2050,2,2)),     # date > 1000 days in future for now so ??? displayed
+            ('Memorial', dt.date(2050,8,1)),
            ]
+
 
 def tick(time1=''):
     # get the current local time from the PC
@@ -21,33 +16,25 @@ def tick(time1=''):
     if time2 != time1:
         time1 = time2
         today = dt.date.today()
-        calendar.config(text=today.strftime("%a, %x"))
-        calendar.grid(row=0, column=0, sticky=tk.W)
+        cal_day.config(text=today.strftime("%A"))
+        cal_day.grid(row=0, column=0, sticky=tk.W)
+        cal_date.config(text=today.strftime("%x"))
+        cal_date.grid(row=0, column=1, sticky=tk.E)
         clock.config(text=time2)
-        clock.grid(row=0, column=1, sticky=tk.E)
-        for i in range(len(go_lives)): # using regular iteration would be hard 
-                                # since I need next item as well
-                                # this will break after academic go-live,
-                                # but will do for now
-            if go_lives[i][1] > today:
-                countdown1 = go_lives[i][1] - today
-                h1 = go_lives[i][0]
-                countdown2 = go_lives[i+1][1] - today
-                h2 = go_lives[i+1][0]
+        clock.grid(row=1, columnspan=2)
+        for site in go_lives:
+            if site[1] > today:
+                countdown = site[1] - today
+                pod = site[0]
                 break
-        cd1 = str(countdown1.days)
-        cd2 = str(countdown2.days)
-        hosp1.config(text= h1 + ' goes live in:')
-        hosp1.grid(row=1, columnspan=2)
-        if cd1=="1":
-            count1.config(text= cd1 + ' day')
+        cd_line.grid(row=2, columnspan=2)
+        if countdown.days == 1:
+            cd_line.config(text=  pod + ' goes live in: 1 day')
+        elif countdown.days > 1000:
+            cd_line.config(text= pod + ' goes live in: ??? days')
         else:
-            count1.config(text=cd1 + ' days')
-        count1.grid(row=2,columnspan=2)
-        hosp2.config(text= h2 + ' goes live in:')
-        hosp2.grid(row=3, columnspan=2)
-        count2.config(text= cd2 + ' days')
-        count2.grid(row=4,columnspan=2)        
+            cd_line.config(text= pod + ' goes live in: ' + str(countdown.days) + ' days')
+
     # calls itself every 200 milliseconds
     # to update the time display as needed
     clock.after(200, tick)
@@ -55,13 +42,12 @@ root = tk.Tk()
 root.configure(background='#7ea0d6')
 root.geometry(window_size)
 root.columnconfigure(1, weight=1)
-root.title('HIP Countdown Clock')
-calendar = tk.Label(root, font=('helvetica', 32), bg='#7ea0d6')
-clock = tk.Label(root, font=('helvetica', 80, 'bold'), bg='#7ea0d6', fg='blue4')
-hosp1 = tk.Label(root, font=('helvetica', 24, 'bold'), bg='#7ea0d6')
-count1 = tk.Label(root, font=('helvetica', 48, 'bold'), bg='#7ea0d6')
-hosp2 = tk.Label(root, font=('helvetica', 24, 'bold'), bg='#7ea0d6')
-count2 = tk.Label(root, font=('helvetica', 48, 'bold'), bg='#7ea0d6')
+root.title('KFW Clock')
+cal_day = tk.Label(root, font=('helvetica', 48), bg='#7ea0d6')
+cal_date = tk.Label(root, font=('helvetica', 48), bg='#7ea0d6')
+clock = tk.Label(root, font=('helvetica', 256, 'bold'), bg='#7ea0d6', fg='blue4')
+cd_line = tk.Label(root, font=('helvetica', 32), bg='#7ea0d6')
+
 
 # clock.pack(fill='both', expand=1)
 tick()
