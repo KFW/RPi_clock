fe2b355a909aa5e268f3479fb9d114137a666afc
Keith F. Woeltje <comm@woeltje.org>
2023-02-12 19:21:18 -0600

    fix to update event strings properly

--- rpcd.py
+++ rpcd.py
@@ -4,9 +4,11 @@ import datetime as dt
 
 # OK it may be lazy, and sloppy, but using globals in a program 
 # this short just makes life easier
-events = []
-event1string = "-"
-event2string = "-"
+# events = []
+# event1string = "-"
+# event2string = "-"
+# initialize = True
+today = 0
 
 window_size = '800x480'
 
@@ -20,13 +22,13 @@ def msg_string(event_name, countdown):
     else:
         return event_name + ' is in: ' + str(countdown.days) + ' days'
 
-def get_event_strings():
-    global events
-    global event1string
-    global event2string
-
-    event1string = " "
-    event2string = "-----------"
+def get_event_strings(events, today):
+    # global events
+    # global event1string
+    # global event2string
+ 
+    event1string = "x"
+    event2string = "xx"
     for index, event in enumerate(events):
         if event[1] >= today:
             countdown = event[1] - today
@@ -41,18 +43,17 @@ def get_event_strings():
 
 # update events list; handle errors if file not present by countdown to upcoming years
 def get_events():
-    global events 
-    
+    # global events 
+    events = []
     try:
         with open('events.txt') as f:   # 'events.txt must be in same directory as program file
             for line in f:
                 l = line.strip().split(",")
-                event = (l[0], dt.date(int(l[1]),int(l[2]), int(l[3])))
+                event = (l[0], dt.date(int(l[1]), int(l[2]), int(l[3])))
                 events.append(event)
-        return events
+            return events
     except:
-        events = [  ('2023', dt.date(2023,1,1)),
-                    ('2024', dt.date(2024,1,1)),
+        events = [  ('2024', dt.date(2024,1,1)),
                     ('2025', dt.date(2025,1,1)),
                     ('2026', dt.date(2026,1,1)),
                     ('2027', dt.date(2027,1,1)),
@@ -64,12 +65,23 @@ def get_events():
 
 def tick():
     global today
-    global events
-    
+    # global events
+    # global initialize
+
+    # if initialize:
+    #     initialize = False
+    #     today = dt.date.today()
+    #     events = get_events()
+    #     event1string, event2string = get_event_strings(events, today)
     # check date - if new date then reload events file
+
+    # event1string = " "
+    # event2string = "-----------"
+    
     if today != dt.date.today():
         today = dt.date.today()
-        get_events()
+        events = get_events()
+        event1string, event2string = get_event_strings(events, today)
 
     # get the current local time from the PC
     time_now = time.strftime('%H:%M')
@@ -93,7 +105,9 @@ def tick():
     #             event2string = "----------"
     #         break
     cd_line1.grid(row=2, columnspan=2)
+    print(event1string)
     cd_line1.config(text = event1string)
+    print(event2string)
     cd_line2.grid(row=3, columnspan=2)
     cd_line2.config(text = event2string)
 
@@ -114,9 +128,9 @@ cd_line2 = tk.Label(root, font=('helvetica', 24), bg='#7ea0d6')
 
 
 # initialize
-today = dt.date.today()
-get_events()
-get_event_strings()
+# today = dt.date.today()
+# get_events()
+# get_event_strings()
 
 # Not actually sure at this point why it keeps looping like it does
 # I don't recall where I saw the example code
