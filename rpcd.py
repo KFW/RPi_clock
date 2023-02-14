9d34d7b2d3d6c2901c846acd37fd879fbe60fd2b
Keith F. Woeltje <comm@woeltje.org>
2023-02-13 20:03:19 -0600

    Simplify - no countdown. Just clock and fixed msg

--- rpcd.py
+++ rpcd.py
@@ -2,65 +2,12 @@ import time
 import tkinter as tk
 import datetime as dt
 
-today = 0
+message1string = 'Clinical Informatics'
+message2string = 'Froedtert & MCW'
 
 window_size = '800x480'
 
-def msg_string(event_name, countdown):
-    if countdown.days == 0:
-        return event_name + 'is today!'
-    elif countdown.days == 1:
-        return event_name + ' is in: 1 day'
-    elif countdown.days > 1000:
-        return event_name + ' is in: ??? days'
-    else:
-        return event_name + ' is in: ' + str(countdown.days) + ' days'
-
-def get_event_strings(events, today):
- 
-    event1string = "x"
-    event2string = "xx"
-    for index, event in enumerate(events):
-        if event[1] >= today:
-            countdown = event[1] - today
-            event_name = event[0]
-            event1string = msg_string(event_name, countdown)
-            if (index + 1) < len(events): # there are additional events
-                countdown = events[index + 1][1] - today
-                event_name = events[index + 1][0]
-                event2string = msg_string(event_name, countdown)
-            break
-    return (event1string, event2string)
-
-# update events list; handle errors if file not present by countdown to upcoming years
-def get_events():
-
-    events = []
-    try:
-        with open('events.txt') as f:   # 'events.txt must be in same directory as program file
-            for line in f:
-                l = line.strip().split(",")
-                event = (l[0], dt.date(int(l[1]), int(l[2]), int(l[3])))
-                events.append(event)
-            return events
-    except:
-        events = [  ('2024', dt.date(2024,1,1)),
-                    ('2025', dt.date(2025,1,1)),
-                    ('2026', dt.date(2026,1,1)),
-                    ('2027', dt.date(2027,1,1)),
-                    ('2028', dt.date(2028,1,1)),
-                    ('2029', dt.date(2029,1,1)),
-                    ('2030', dt.date(2030,1,1)),
-                 ]
-        return events
-
 def tick():
-    global today
-    
-    if today != dt.date.today():
-        today = dt.date.today()
-        events = get_events()
-        event1string, event2string = get_event_strings(events, today)
 
     # get the current local time from the PC
     time_now = time.strftime('%H:%M')
@@ -73,9 +20,9 @@ def tick():
     clock.grid(row=1, columnspan=2)
 
     cd_line1.grid(row=2, columnspan=2)
-    cd_line1.config(text = event1string)
+    cd_line1.config(text = message1string)
     cd_line2.grid(row=3, columnspan=2)
-    cd_line2.config(text = event2string)
+    cd_line2.config(text = message2string)
 
     # calls itself every 200 milliseconds
     # to update the time display as needed
